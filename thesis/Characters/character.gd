82bb514325735ba5f7a6a1b23c5925110958f744
HarrisonHallett <104926467+HarrisonHallett@users.noreply.github.com>
2024-12-13 14:38:15 -0500

    Created player node and added walk and idle animations to the player scene

--- thesis/Characters/character.gd
+++ thesis/Characters/character.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 class_name Character
 
+
+
 const FRICTION: float = 0.15 
 
 @export var acceleration: int = 40
 @export var max_speed: int = 100
 
+@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+
 
 var mov_direction: Vector2 = Vector2.ZERO
-#var velocity: Vector2 = Vector2.ZERO
+var state_machine
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	state_machine = get_node("FiniteStateMachine")
+
+func _physics_process(_delta: float) -> void:
 	if velocity.length() > 0:
 		move_and_slide()
 	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
